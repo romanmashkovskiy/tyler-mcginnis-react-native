1a5c694f757ffd8995077e0d00a0d7a8466469db
Roman Mashkovskiy <ra.mashkovskiy@gmail.com>
2019-01-23 21:27:25 +0200

    AddEntry TextButton

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import {getMetricMetaInfo, timeToString} from '../utils/helpers';
 import UdaciSlider from './UdaciSlider';
 import UdaciStepper from './UdaciStepper';
 import DateHeader from './DateHeader';
+import {Ionicons} from '@expo/vector-icons';
+import TextButton from './TextButton';
 
 function SubmitBtn({onPress}) {
     return (
@@ -71,8 +73,27 @@ export default class AddEntry extends Component {
         }));
     };
 
+    reset = () => {
+        const key = timeToString();
+    };
+
     render() {
         const metaInfo = getMetricMetaInfo();
+
+        if (this.props.alreadyLogged) {
+            return (
+                <View>
+                    <Ionicons
+                        name='md-happy'
+                        size={100}
+                    />
+                    <Text>
+                        You have already logged your information today
+                    </Text>
+                    <TextButton onPress={this.reset}>Reset</TextButton>
+                </View>
+            );
+        }
 
         return (
             <View>
