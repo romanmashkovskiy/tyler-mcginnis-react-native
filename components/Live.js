dad06e28344ae4a133709cfa595ca252f74f8da5
Roman Mashkovskiy <ra.mashkovskiy@gmail.com>
2019-01-29 12:16:12 +0200

    undetermined + denied geolocation

--- components/Live.js
+++ components/Live.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
-import {View, Text, ActivityIndicator} from 'react-native';
+import {View, Text, ActivityIndicator, TouchableOpacity, StyleSheet} from 'react-native';
+import {Entypo} from '@expo/vector-icons';
+import {purple, white} from '../utils/colors';
+import TextButton from "./TextButton";
 
 class Live extends Component {
     state = {
         coords: null,
-        status: null,
+        status: 'denied',
         direction: ''
+    };
+
+    askPermission = () => {
+
     };
 
     render() {
@@ -19,16 +26,21 @@ class Live extends Component {
 
         if (status === 'denied') {
             return (
-                <View>
-                    <Text>denied</Text>
+                <View style={styles.center}>
+                    <Entypo name='warning' size={50}/>
+                    <Text>You denied your location. Please visit your settings to fix this.</Text>
                 </View>
             );
         }
 
         if (status === 'undetermined') {
             return (
-                <View>
-                    <Text>undetermined</Text>
+                <View style={styles.center}>
+                    <Entypo name='warning' size={50}/>
+                    <Text>You need to enable location services for this app</Text>
+                    <TouchableOpacity style={styles.button} onPress={this.askPermission}>
+                        <Text style={styles.buttonText}>Enable</Text>
+                    </TouchableOpacity>
                 </View>
             );
         }
@@ -41,5 +53,23 @@ class Live extends Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {},
+    center: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        backgroundColor: purple,
+        borderRadius: 4,
+        padding: 10,
+        margin: 20,
+    },
+    buttonText: {
+        color: white,
+    }
+});
 
 export default Live;
